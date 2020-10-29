4f9c7a04ab372947dc4a0f0936bafe3abc25cf2b
Andrii Lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-10-29 18:32:17 +0200

    Adds task-2

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,1 +1,16 @@
-console.log('task2');
+import users from './usersArr.js';
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    resolve(
+      allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+      ),
+    );
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
